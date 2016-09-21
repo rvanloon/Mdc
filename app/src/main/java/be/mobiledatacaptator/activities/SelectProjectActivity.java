e9ca4c5b0e555644d8b4a34c8c25971b96645260
rvanloon <robrecht.vanloon@gmail.com>
2016-09-21 17:00:18 +0200

    start LocalFileSystemDAO

--- app/src/main/java/be/mobiledatacaptator/activities/SelectProjectActivity.java
+++ app/src/main/java/be/mobiledatacaptator/activities/SelectProjectActivity.java
@@ -18,7 +18,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AbsListView;
---
+++ app/src/main/java/be/mobiledatacaptator/dao/LocalFileSystemDao.java
@@ -0,0 +1,116 @@
+package be.mobiledatacaptator.dao;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by rvanloon on 21/09/2016.
+ * Files are stored on the local file system
+ * <p>
+ * the path in the methods is always relative to a 'root'
+ * (Was originaly for dropbox, and the root was set in the account)
+ * So now, for each path, the root has to be added.
+ */
+
+public class LocalFileSystemDao implements IMdcDao {
+
+    private String rootPath = "";
+
+    private String getFullPath(String path) {
+        int numberSlashes = 0;
+        numberSlashes += rootPath.endsWith("/") ? 1 : 0;
+        numberSlashes += path.startsWith("/") ? 1 : 0;
+
+        if (numberSlashes == 0) {
+            return rootPath + "/" + path;
+        } else if (numberSlashes == 2) {
+            return rootPath.substring(0, rootPath.length() - 1) + path;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String getFilecontent(String path) throws IOException {
+        File file = new File(getFullPath(path));
+        StringBuilder builder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String str;
+        while ((str = bufferedReader.readLine()) != null) {
+            builder.append(str);
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public List<String> getAllFilesFromPathWithExtension(String path, String extension, Boolean returnExtension) throws Exception {
+        List<String> folderContent = new ArrayList<>();
+        File[] rawFiles = new File(path).listFiles();
+        for (File f : rawFiles) {
+            String fileName = f.getName();
+            if (fileName.endsWith(extension)) {
+                fileName = returnExtension ? fileName : fileName.substring(0, fileName.length() - extension.length());
+                folderContent.add(fileName);
+            }
+        }
+        return folderContent;
+    }
+
+    @Override
+    public boolean existsFile(String path) throws Exception {
+        return new File(path).exists();
+    }
+
+    @Override
+    public void delete(String path) throws Exception {
+        File file = new File(path);
+        file.delete();
+    }
+
+    @Override
+    public void saveFile(String path, File file) throws Exception {
+        InputStream inputStream = new FileInputStream(file);
+        OutputStream outputStream = new FileOutputStream(path);
+
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = inputStream.read(buf))>0){
+            outputStream.write(buf,0,len);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+    @Override
+    public void saveStringToFile(String path, String string) throws Exception {
+
+    }
+
+    @Override
+    public void appendStringToFile(String path, String string) throws Exception {
+
+    }
+
+    @Override
+    public Bitmap getBitmapFromFile(String path) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void dumpToSd() throws Exception {
+
+    }
+}
